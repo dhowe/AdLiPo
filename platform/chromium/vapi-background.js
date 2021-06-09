a81269378f163dfc2e64ff7fb6bdbb798a3017a4
JohnC <real.john.chueng@gmail.com>
2021-06-09 20:29:25 +0800

    basic text injection

--- platform/chromium/vapi-background.js
+++ platform/chromium/vapi-background.js
@@ -110,6 +110,8 @@ const jsTamplateToInsert = `
             injectedBG.style.position = "relative";
             if (dbug) console.log("replacing ", node, "with", injectedBG);
             node.parentNode.replaceChild(injectedBG, node);
+            //append text
+            appendText("This is a test centence. This is another one.", injectedBG);
         } else {
             // div, li, etc
             injectedBG.style.position = "relative";
@@ -117,6 +119,8 @@ const jsTamplateToInsert = `
             injectedBG.style.left = "opx";
             if (dbug) console.log("replacing ", node, "with", injectedBG);
             node.parentNode.replaceChild(injectedBG, node);
+            //append text
+            appendText("This is a test centence. This is another one.", injectedBG);
         }
         
     }
@@ -136,6 +140,115 @@ const jsTamplateToInsert = `
     function getColor() {
         let pool = ['#4484A4', '#A2B6C0', '#889D59', '#CF8D2F', '#C55532'];
         return pool[Math.floor(Math.random() * pool.length)];
+    }
+
+    function appendText(textContent, element) {
+        // textContent should be a string 
+        // element should be a DOM element
+        if (!textContent || !element) {
+            console.error("doLayout requires string and DOM element");
+            return;
+        }
+        if (typeof textContent !== "string") textContent = textContent.toString();
+        if (!(typeof HTMLElement === "object" ? element instanceof HTMLElement : typeof element === "object" && element !== null && element.nodeType === 1 && typeof element.nodeName==="string")) {
+            console.error("doLayout expects a DOM element, get ", element, "instead");
+            return;
+        }
+        const width = element.style.width;
+        const height = element.style.height;
+        if (!width || !height || !/^[0-9\\.]+px$/.test(width) || !/^[0-9\\.]+px$/.test(height) ) {
+            console.error("doLayout expects a DOM with fixed width and height");
+            return;
+        }
+        //clear target
+        while(element.firstChild){
+            element.removeChild(element.lastChild);
+        }
+        const font = element.style.fontFamily || 'Bench9'; // a string representing the font name
+        const textAlign = element.style.textAlign || 'left'; // a string representing alignment
+        const wordBreak = element.style.wordBreak || 'break-word'; 
+        const lineHeight = element.style.lineHeight || 'normal';
+        const padding = element.style.padding || "0";
+        //deal with possible orphan
+        //textContent = textContent.replace(/ +/g, "\\u00a0");// works but not good: 1.jam all letter into one "word"; 2.space might occur at the start of the line
+        // const textGroup = textContent.split("\\n");
+        // const newtextGroup = []
+        // textGroup.forEach(txt => {
+        //     const textArr = txt.trim().split(" ");
+        //     const final2 = textArr.splice(textArr.length - 2, 2);
+        //     textArr.push(final2.join("\\u00a0"));
+        //     newtextGroup.push(textArr.join(" "));
+        // });
+        // textContent = newtextGroup.length === 1 ? newtextGroup[0] : newtextGroup.join("\\n"); // in extremely rare case create word break ...
+        
+        //compute fontSize
+        const fontSize = computeFontSize(textContent, width, height, font, textAlign, wordBreak, lineHeight, padding);
+        //set to the div
+        element.style.fontFamily = font;
+        element.style.textAlign = textAlign;
+        element.style.fontSize = fontSize;
+        element.style.wordBreak = wordBreak;
+        element.style.padding = padding;
+        element.style.lineHeight = lineHeight;
+        //use a inline element to wrap it, so it can be in the middle
+        element.style.display = "table";
+        const spanElement = document.createElement("span");
+        spanElement.style.display = "table-cell";
+        spanElement.style.verticalAlign = "middle";
+        spanElement.innerText = textContent;
+        element.appendChild(spanElement);
+    }
+
+    function computeFontSize(textContent, width, height, font, textAlign, wordBreak, lineHeight, padding, tryLimit) {
+        // tem element version
+        textContent = textContent.trim();
+        let css = {}, cssStr;
+        const getRealHeight = function() {
+            testEl.style.fontSize = guess+ 'px';
+            document.body.appendChild(testEl);
+            realHeight = testEl.offsetHeight;
+            testEl.parentNode.removeChild(testEl);
+        }
+        const limit = tryLimit || 100;
+        const targetWidth = parseFloat(width.slice(0,-1).slice(0,-1));
+        const targetHeight = parseFloat(height.slice(0,-1).slice(0,-1));
+        const contentLength = textContent.length;
+        const testEl = document.createElement("div");
+        let lastDirection;
+        const last5 = [];
+        let tries = 0;
+        let cross = 0;
+        //first using math to guess
+        let guess = (Math.sqrt(targetWidth * targetHeight / contentLength)); // in px
+        cssStr = 'height: auto;display: block!important;width: ' + width + '!important;padding: ' + padding + ';font-size: ' + guess + 'px;font-family: ' + font + ';text-align: ' + textAlign + ';word-break: ' + wordBreak + ';line-height: ' + lineHeight + ';';
+        //trying
+        testEl.setAttribute("style", cssStr);
+        testEl.innerText = textContent;
+        document.body.appendChild(testEl);
+        let realHeight = testEl.offsetHeight;
+        testEl.parentNode.removeChild(testEl);
+        while (tries < limit && Math.abs(realHeight - targetHeight) > Math.max(0.05*targetHeight, 5)) {
+            let gap = targetHeight - realHeight;
+            if (gap > 0 !== lastDirection) cross ++;
+            if (cross > 3) {
+                guess = last5.sort((a, b) => {return b - a})[0];
+                getRealHeight();
+                break;
+            }
+            lastDirection = gap > 0;
+            guess += (gap * guess) / realHeight;
+            getRealHeight();
+            if (last5.length >= 5) last5.shift();
+            last5.push(guess);
+            tries++;
+        }
+        guess = guess.toFixed(2);
+        while (realHeight > targetHeight) {
+            guess -= 0.1; // min 0.1 px
+            getRealHeight();
+        }
+        
+        return guess + 'px';
     }
 
     window.addEventListener("load", findAndProcess);
