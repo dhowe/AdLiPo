7a6b01c1f05d4b2bbdf37186c92ae365c3c25d40
John Cheung <61215358+Real-John-Cheung@users.noreply.github.com>
2021-07-16 22:02:56 +0800

    more locales

--- tools/copy-common-files.sh
+++ tools/copy-common-files.sh
@@ -12,14 +12,16 @@ cp -R src/js                       $DES/
 cp -R src/lib                      $DES/
 cp -R src/web_accessible_resources $DES/
 # cp -R src/_locales               $DES/
-# AdLiPo
-cp -R AdLiPo/_locales              $DES/
-cp -R AdLiPo/src/js                $DES/
-cp -R AdLiPo/lib/*.js              $DES/js/
-cp -R AdLiPo/AdLiPoImage           $DES
 
 cp src/*.html                      $DES/
 cp platform/chromium/*.js          $DES/js/
 cp platform/chromium/*.html        $DES/
 cp platform/chromium/*.json        $DES/
 cp LICENSE.txt                     $DES/
+
+# AdLiPo
+cp -R AdLiPo/_locales              $DES/
+cp -R AdLiPo/src/js                $DES/
+cp -R AdLiPo/lib/*.js              $DES/js/
+cp -R AdLiPo/AdLiPoImage           $DES
+yes | cp AdLiPo/*.html             $DES/
